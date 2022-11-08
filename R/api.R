a9ac708c5a684d6fc190a42b0cdbe0e20f7f5bce
Jens Laufer <jenslaufer@gmail.com>
2022-11-08 07:49:41 +0100

    added outlier_only

--- R/api.R
+++ R/api.R
@@ -200,11 +200,11 @@ qualitative_plot <-
     if (!(variable3 %>% is.null()) &&
         !(variable4 %>% is.null())) {
       data <- data %>%
-        group_by(!!sym(variable2),!!sym(variable3),!!sym(variable4))
+        group_by(!!sym(variable2), !!sym(variable3), !!sym(variable4))
     } else if (!(variable3 %>% is.null()) &&
                variable4 %>% is.null()) {
       data <- data %>%
-        group_by(!!sym(variable2),!!sym(variable3))
+        group_by(!!sym(variable2), !!sym(variable3))
     } else if (!(variable2 %>% is.null()) &&
                variable3 %>% is.null() &&
                variable4 %>% is.null()) {
@@ -219,18 +219,23 @@ qualitative_plot <-
     if (!(variable3 %>% is.null()) &&
         !(variable4 %>% is.null())) {
       data <-
-        data %>% group_by(!!sym(variable1),!!sym(variable2),!!sym(variable3),!!sym(variable4),
+        data %>% group_by(!!sym(variable1),
+                          !!sym(variable2),
+                          !!sym(variable3),
+                          !!sym(variable4),
                           total)
     } else if (!(variable3 %>% is.null()) &&
                variable4 %>% is.null()) {
       data <-
-        data %>% group_by(!!sym(variable1),!!sym(variable2),!!sym(variable3),
+        data %>% group_by(!!sym(variable1),
+                          !!sym(variable2),
+                          !!sym(variable3),
                           total)
     } else if (!(variable2 %>% is.null()) &&
                variable3 %>% is.null() &&
                variable4 %>% is.null()) {
       data <-
-        data %>% group_by(!!sym(variable1),!!sym(variable2), total)
+        data %>% group_by(!!sym(variable1), !!sym(variable2), total)
     }
     
     
@@ -297,7 +302,9 @@ quantitative_qualitative_plot <-
       plot <- data %>%
         ggplot(aes(
           x = reorder_within(
-            !!sym(qualitativeVariable1),!!sym(quantitiveVariable),!!sym(qualitativeVariable2),
+            !!sym(qualitativeVariable1),
+            !!sym(quantitiveVariable),
+            !!sym(qualitativeVariable2),
             fun = median
           ),
           y = !!sym(quantitiveVariable)
@@ -307,9 +314,11 @@ quantitative_qualitative_plot <-
       plot <- data %>%
         ggplot(aes(
           x = reorder_within(
-            !!sym(qualitativeVariable1),!!sym(quantitiveVariable),
+            !!sym(qualitativeVariable1),
+            !!sym(quantitiveVariable),
             list(
-              !!sym(qualitativeVariable2),!!sym(qualitativeVariable3)
+              !!sym(qualitativeVariable2),
+              !!sym(qualitativeVariable3)
             ),
             fun = median
           ),
@@ -319,7 +328,8 @@ quantitative_qualitative_plot <-
       plot <- data %>%
         ggplot(aes(
           x = reorder(
-            !!sym(qualitativeVariable1),!!sym(quantitiveVariable),
+            !!sym(qualitativeVariable1),
+            !!sym(quantitiveVariable),
             fun = median
           ),
           y = !!sym(quantitiveVariable)
@@ -360,7 +370,7 @@ quantitative_qualitative_plot <-
   }
 
 univariate_plot <-
-  function(data, variable, outlier_treatment = "") {
+  function(data, variable, outlier_treatment = NULL) {
     data <-
       data %>% .outlier_handling(data, c(variable), outlier_treatment)
     
@@ -388,10 +398,10 @@ univariate_plot <-
     }
   }
 
-univariate_plots <- function(data, outlier_treatment = "") {
+univariate_plots <- function(data, outlier_treatment = NULL) {
   data %>%
     colnames() %>%
-    map(~ data %>% univariate_plot(..1, outlier_treatment))
+    map( ~ data %>% univariate_plot(..1, outlier_treatment))
 }
 
 bivariate_plot <-
@@ -400,7 +410,7 @@ bivariate_plot <-
            variable2,
            variable1Scale = "identity",
            variable2Scale = "identity",
-           outlier_treatment = "") {
+           outlier_treatment = NULL) {
     data <-
       data %>% .outlier_handling(data, c(variable1, variable2), outlier_treatment)
     
@@ -428,16 +438,16 @@ bivariate_plot <-
     }
   }
 
-bivariate_plots <- function(data, outlier_treatment = "") {
+bivariate_plots <- function(data, outlier_treatment = NULL) {
   data %>%
     .var_pairs() %>%
-    pmap(~ data %>% bivariate_plot(..1, ..2, outlier_treatment))
+    pmap( ~ data %>% bivariate_plot(..1, ..2, outlier_treatment))
 }
 
 .outlier_handling <- function(data, columns, outlier_treatment) {
   if (outlier_treatment == "outlier_removed") {
     data <- data %>% rm_tukey_outliers(columns)
-  } else {
+  } else if (outlier_treatment == "outlier_only") {
     data_without_outliers <- data %>% rm_tukey_outliers(columns)
     data <- data %>% anti_join(data_without_outliers)
   }
